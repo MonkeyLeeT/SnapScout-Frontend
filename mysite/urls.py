71ceed6d8746398b998a424994681cb50cfb92b3
Ted Li <tl2493@columbia.edu>
2015-04-28 11:51:43 +0800

    Update project structure.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,14 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from views import hello, index, getPhotos, getSinglePhoto, postComment, postRating, searchPosition, recommendation, \
-    signup, getRecommendationPhotos, getCluster, cluster, recPlace, getRecPlace
-from views import login
+from views import *
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', index),
